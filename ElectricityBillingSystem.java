3d761c6dae20fa912bbefc5913aee86dd32e31b2
Dev Ayush <143614582+Explicable11@users.noreply.github.com>
2025-07-27 17:11:31 +0530

    Update ElectricityBillingSystem.java

--- ElectricityBillingSystem.java
+++ ElectricityBillingSystem.java
@@ -16,7 +16,7 @@ public class ElectricityBillingSystem {
 
         public Conn() {
             try {
-                // Setup The Connection to the database
+                // Setup The Connection To the database
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 c = DriverManager.getConnection("jdbc:mysql://localhost:3306/electricity_billing_system", "root", "rootroot");
                 s = c.createStatement();
